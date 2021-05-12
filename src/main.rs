ce5ee19c9e137fce75355970213b75ac846921d4
Piyush Mishra <piyush.raj.kit@gmail.com>
2021-05-12 14:01:36 +0530

    rate limit

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@
 use actix_web::{App, Error, HttpRequest, HttpResponse, HttpServer, middleware::Logger, web};
 use actix_files as fs;
 use actix_web_actors::ws;
+use actix_ratelimit::{RateLimiter, MemoryStore, MemoryStoreActor};
 use ws_sansad::WsSansad;
 
 mod config;
@@ -24,11 +25,17 @@ mod validator;
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
-
+    let store = MemoryStore::new();
     let config = config::Config::new();
     let static_path = config.static_path;
     HttpServer::new(move || {
         App::new()
+        .wrap(
+            RateLimiter::new(
+            MemoryStoreActor::from(store.clone()).start())
+                .with_interval(std::time::Duration::from_secs(60))
+                .with_max_requests(200)
+        )
         .wrap(Logger::new("%t [%{x-forwarded-for}i] %s %{User-Agent}i %r"))
         .service(web::resource("/ws/").route(web::get().to(ws_index)))
         .service(fs::Files::new("/", &static_path).index_file("index.html"))
