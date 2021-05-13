650b1bbd4d30d0b808b6d5bf852fe6ce2143454d
Rajnish Mishra <contact.rajnishmishra@gmail.com>
2021-05-13 15:28:40 +0530

    minor fixes

--- src/errors/user_kunjika_error.rs
+++ src/errors/user_kunjika_error.rs
@@ -1,18 +1,18 @@
 /*
-    This file is part of Tarangam.
+    This file is part of Lupt.
 
-    Tarangam is free software: you can redistribute it and/or modify
+    Lupt is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Tarangam is distributed in the hope that it will be useful,
+    Lupt is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Tarangam.  If not, see <https://www.gnu.org/licenses/>
+    along with Lupt.  If not, see <https://www.gnu.org/licenses/>
 */
 
 use std::fmt;
