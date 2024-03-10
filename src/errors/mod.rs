481c89acecf08846b2d8ba5fe63ff09172ee655c
PiyushXCoder <piyush.raj.kit@gmail.com>
2024-03-10 09:36:42 +0530

    Fixed GIF

--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -18,5 +18,5 @@
 mod kaksh_full_error;
 mod user_kunjika_error;
 
-pub use kaksh_full_error::KakshFullError;
-pub use user_kunjika_error::AlreadyExistError;
+// pub use kaksh_full_error::KakshFullError;
+// pub use user_kunjika_error::AlreadyExistError;
